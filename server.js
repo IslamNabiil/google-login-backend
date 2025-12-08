1d8e91d3fecffd919f194f3ad8625940eec1a0ba
IslamNabiil <eslamoka1123@gmail.com>
2025-12-09 00:14:32 +0200

    FIX: Update CORS origin to live frontend Vercel domain

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 // أ) CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://google-login-frontend-coral.vercel.app",
     credentials: true,
   })
 );
